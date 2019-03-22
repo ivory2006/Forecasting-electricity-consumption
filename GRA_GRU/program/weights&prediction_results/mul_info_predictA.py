7180a7bfcd33d6c759b83922851c86b0edb3daaa
ivory2006 <30310430+ivory2006@users.noreply.github.com>
2019-03-22 13:49:47 +0800

    Use mutual information to determine weights and get predictions
    Use mutual information to determine weights and get predictions

---
+++ GRA_GRU/program/weights&prediction_results/mul_info_predictA.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from pandas import read_csv
+from pandas import DataFrame
+from numpy import *
+import numpy as np
+import mul_info_weightA
+
+def mul_info_predict(gru_test_file,le_test_file):
+    gru_test_dataset = read_csv(gru_test_file, header=0, index_col=None)
+    reality = gru_test_dataset.values[:,0]
+    gru_predict = gru_test_dataset.values[:, 1]
+
+    le_test_dataset = read_csv(le_test_file, header=0, index_col=None)
+    le_predict = le_test_dataset.values[:, 1]
+
+    mul_info_predict = np.array(gru_predict) * mul_info_weightA.gru_weight + np.array(le_predict) * mul_info_weightA.le_weight
+    mape_list = []
+    acc_list  = []
+    mape = 0
+    for i in range(len(mul_info_predict)):
+        mape_list.append(abs(mul_info_predict[i]-reality[i])/reality[i])
+        acc_list.append(1-abs(mul_info_predict[i]-reality[i])/reality[i])
+        mape += abs(mul_info_predict[i]-reality[i])/reality[i]
+    mape = mape/len(mul_info_predict)
+    print(mape)
+    print(np.var(acc_list))
+
+if __name__ == "__main__":
+    mul_info_predict("experimentA_GRU_result.csv","experimentA_LE_result.csv")
